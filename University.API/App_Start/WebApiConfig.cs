94642b0b72af8c1c278486d8893888f222881f95
jo-munoz <jo-munoz@hotmail.com>
2021-04-13 09:56:41 -0500

    Video 14 terminado - Habilitando solicitudes cross-origin en ASP.NET Web API 2

--- University.API/App_Start/WebApiConfig.cs
+++ University.API/App_Start/WebApiConfig.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Cors;
 using University.API.Controllers;
 
 namespace University.API
@@ -7,7 +8,11 @@ namespace University.API
     {
         public static void Register(HttpConfiguration config)
         {
-            // Configuración y servicios de API web
+            // Configuración y servicios de API web            
+            var enableCorsAttribute = new EnableCorsAttribute("*", 
+                "Origin, Content-Type, Accept",
+                "GET, PUT, POST, DELETE, OPTIONS");
+            config.EnableCors(enableCorsAttribute);
 
             // Rutas de API web
             config.MapHttpAttributeRoutes();
